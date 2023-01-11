28d81365b7b2c14355af4567dd43e5fb4af94f32
Uruvsereg <uruvsereg@hotmail.com>
2023-01-11 14:10:18 -0500

    Operaciones

---
+++ operaciones.js
@@ -0,0 +1,12 @@
+console.log(3+5);
+console.log(14-7);
+console.log(8*6);
+console.log(30/9);
+console.log(95%4);
+console.log(3+5*2-8);
+console.log((3+5)*2-8);
+console.log(3**3);
+console.log('Hola'+'Uru');
+var nombre='Uru';
+var saludo='Hola'+nombre;
+console.log(saludo);
